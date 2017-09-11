12a505d84a0ebc4d0f03206521cbb25abaf96c4b
Eduardo Sutil <sutil.edu@gmail.com>
2017-09-11 15:49:42 -0300

    primeira fase: colocando boards para destacar os IPs

--- www/js/app/subnets.js
+++ www/js/app/subnets.js
@@ -1,6 +1,5 @@
 angular.module('netanimations.subnets', [])
 .controller('SubnetsCtrl', function($state, $scope, $ionicPopup, $translate, $compile) {
-  
   $scope.end = false;
   
     var tl = new TimelineLite();
@@ -39,20 +38,26 @@ angular.module('netanimations.subnets', [])
     var subnets = '.subnets';
     var board = '.subnets-board'
     var widthScreen = window.screen.width;
+    var heightScreen = window.screen.height;
     var subnetsWidth = window.screen.width*2.5;
     var slideImage = widthScreen*1.52;
 
-    var imgSubnets = document.getElementById("subnets");
     var center = ((subnetsWidth/2)-(widthScreen/2))*-1;
     var left = 0;
     var right= (subnetsWidth-widthScreen)*-1;
 
-    tl.set(subnets, {width:subnetsWidth}).set(subnets, {y:0}).set(subnets, {x:center});
-    console.log("tela", widthScreen);
-    console.log("img", subnetsWidth);
-    console.log("center", center); 
+    var boardWidth = widthScreen*0.85;
+    var boardHeight = heightScreen*0.10
 
-    tl.set(board, {width: 0});
+    tl.set(subnets, {width:subnetsWidth}).set(subnets, {y:0}).set(subnets, {x:center});
+
+    tl.set(board, {
+      width: boardWidth, 
+      x:getPositionX(boardWidth), 
+      y:heightScreen*0.24,
+      height:boardHeight,
+      opacity:0
+    });
 
     tl.add("step1");
     tl.call( function(){
@@ -60,9 +65,9 @@ angular.module('netanimations.subnets', [])
     });
     tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
     
-    //tl.to(subnets, 1.5, {width: 200});
 
-    
+    tl.to(board, 0.5, {opacity:1});
+    tl.to(board, 0.5, {opacity:0});
 
     tl.add("step2");
     tl.call(function() {
@@ -70,7 +75,14 @@ angular.module('netanimations.subnets', [])
     });
     tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
     
-    tl.to(subnets, 2, {x:left});
+
+    boardHeight = boardHeight+20;
+    tl.to(subnets, 0.5, {x:left});
+    tl.to(board, 0, {height:boardHeight, y:heightScreen*0.33, x:widthScreen*0.05});
+
+    tl.to(board, 0.5,{opacity:1});
+    tl.to(board, 0.5,{opacity:0});
+
     
     tl.add("step3");
     tl.call(function() {
@@ -79,23 +91,19 @@ angular.module('netanimations.subnets', [])
     tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
 
     tl.to(subnets, 2, {x:right});
+    tl.to(board, 0, {x:widthScreen*0.1});
+    tl.to(board, 0.5,{opacity:1});
+    tl.to(board, 0.5,{opacity:0});
 
-    // tl.add("step4");
-    // tl.call(function() {
-    //     commonPopup(tl, $scope, $compile, $translate, $ionicPopup, 'INFO', 'SUBNETS_PRESENTATION_4',"step3");
-    // });
-    // tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
 
-    // tl.add("step5");
-    // tl.call(function() {
-    //     commonPopup(tl, $scope, $compile, $translate, $ionicPopup, 'INFO', 'SUBNETS_PRESENTATION_5',"step4");
-    // });
-    // tl.to('.animationFrame', 0.5, {x: 0}); //dummy step - do not remove
-
-    tl.add("step6");
     tl.call(function() {
-       endPopup(tl,$translate, $ionicPopup, $state, $scope, $compile, 'END', 'SUBNETS_PRESENTATION_6',"step5","step1");
+       endPopup(tl,$translate, $ionicPopup, $state, $scope, $compile, 'END', 'SUBNETS_PRESENTATION_6',"step3","step1");
     });
   
 });
+
+function getPositionX(width){
+    var dif = window.screen.width - width;
+    return dif/2;
+}
 
